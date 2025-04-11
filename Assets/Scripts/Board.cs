a46c2dd537b57b5e5fa2bc29a76596d8b82fcb15
donggeunchoi <cdg01063149045@gmail.com>
2025-04-11 09:37:59 +0900

    Update Board.cs
    카드 배열에 대한 코루틴 코드 이용하여 작성

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,43 +7,60 @@ public class Board : MonoBehaviour
 {
 
     //카드에 대한 변수
-    public Transform cards;
-    public GameObject card;
+    public Transform cards; // 카드들을 넣으 부모 오브젝트
+    public GameObject card; // 카드 프리
 
-    public float spreadTime = 0.5f; //카드가 퍼지는 시간
-    public Vector2 startPosition = new Vector2(0, 0); // 시작 위치
+    public float spreadTime = 0.5f;//카드가 펼쳐지는 시간
+    public Vector2 startPosition = new Vector2(0, 0);//시작하는 위치
 
-    // Start is called before the first frame update
     void Start()
     {
         //랜덤으로 배열을 만들기 위한 변수선언 및 랜덤 뽑기
         int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9 };
         arr = arr.OrderBy(x => Random.Range(0f, 9f)).ToArray();
 
+        StartCoroutine(SpreadCard(arr));
+        //카드 생성 코루틴 시작 (배열 전달)
+
+        GameManager.instance.cardcount = arr.Length;
+        //게임 오브젝트의 카드 카운터는 위에 배열의 길이입니다.
+    }
+
+    //퍼지는 카드에 대한 배열자 
+    IEnumerator SpreadCard(int[] cardArray)
+    {
         for (int i = 0; i < 20; i++)
-        {
-            GameObject go = Instantiate(card, this.transform);
+        { 
+            GameObject go = Instantiate(card, startPosition, Quaternion.identity, this.transform);
             go.transform.parent = cards;
 
-            //카드 배열
-            float x = (i % 4) * 1.4f - 2.1f;
-            float y = (i / 4) * 1.4f - 3.5f;
+            //card스크립트 불러오기, 카드 타입 설정 
+            go.GetComponent<Card>().setting(cardArray[i]);
 
-            go.transform.position = new Vector2(x, y);
+            //목표 위치 계산
+            float targetx = (i % 4) * 1.4f - 2.1f;
+            float targety = (i / 4) * 1.4f - 3.5f;
+            Vector2 targetPos = new Vector2(targetx, targety);
 
+            //이동 애니메이션 시
+            StartCoroutine(MoveCard(go.transform, targetPos));
+            yield return new WaitForSeconds(0.01f);//카드 간 간격
 
-            //card스크립트 불러오기
-            go.GetComponent<Card>().setting(arr[i]);
-            Debug.Log(arr[i]);
-
-
-            GameManager.instance.cardcount = arr.Length;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    //움직이는 카드에 대한 배열자 
+    IEnumerator MoveCard(Transform card, Vector2 targetPos)
     {
-        
+        float timer = 0;
+        Vector2 startPos = card.position;
+
+        while (timer < spreadTime)
+        {
+            timer += Time.deltaTime;
+            card.position = Vector2.Lerp(startPos, targetPos, timer / spreadTime);
+            yield return null;
+        }
+        card.position = targetPos; // 정확한 위치 보정
     }
 }
