7993c73d8182eefe575b27ca80c0a43ebcc21a2d
donggeunchoi <cdg01063149045@gmail.com>
2025-04-11 10:06:00 +0900

    스크립트 추가
    스크립트 추가

---
+++ Assets/Scripts/GameConfig.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConfig : MonoBehaviour
+{
+    public static int level = 1;
+    public static int maxCardNum = 10;
+
+    public static string clearLevelString = "ClearLevel";
+}
